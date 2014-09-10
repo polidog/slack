113d26142a931e213dfeaa8045e511083f7c4971
polidog <polidogs@gmail.com>
2014-09-10 21:24:07 +0900

    urlのリクエストのエラー修正

--- examples/main.go
+++ examples/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
   "github.com/polidog/slack"
+  "log"
   )
 
 func main() {
 	c := slack.Client{}
 	req := slack.MessageRequest{
-    AuthToken: "xxxoxp-2466313728-2466313730-2514368816-382e90"
+    AuthToken: "xxxoxp-2466313728-2466313730-2514368816-382e90",
 		Channel:        "general",
 		Username:          "polidog bot",
-		Message:       "カップラーメンおいしいお"
+		Message:       "カップラーメンおいしいお",
 	}
+
 	if err := c.PostMessage(req); err != nil {
 		log.Printf("Expected no error, but got %q", err)
 	}
